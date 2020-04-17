6b52ced37f8a39c56662496c6fc76547af814d25
j.de.los.rios <j.de.los.rios@accenture.com>
2020-04-17 15:20:17 -0500

    Functional flow

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    let places = staticLoadPlaces();
+    staticLoadPlaces();
 };
 
 function staticLoadPlaces() {
@@ -12,35 +12,35 @@ function staticLoadPlaces() {
             actualLat = position.coords.latitude;
             actualLong = position.coords.longitude;
             console.log('Lat: ', actualLat , '--- Long: ', actualLong);
+            let places = [
+                {
+                    name: 'Magnemite',
+                    location: {
+                        //lat: 4.694047,
+                        lat: actualLat + Math.floor((Math.random() * 10)/100000),
+                        //lng: -74.065458
+                        lng: actualLong + Math.floor((Math.random() * 10)/100000)
+                    },
+                    source: './assets/magnemite/scene.gltf',
+                    scale: '0.3 0.3 0.3'
+                },
+                {
+                    name: 'Chocolate',
+                    location: {
+                        lat: 4.692978,
+                        lat: actualLat + Math.floor((Math.random() * 10)/100000),
+                        //lng: -74.064948
+                        lng: actualLong + Math.floor((Math.random() * 10)/100000)
+                    },
+                    source: './assets/halloween/chocolate.glb',
+                    scale: '0.05 0.05 0.05'
+                }
+            ];
             renderPlaces(places);
         }, err => {
             console.log(err);
         });
     }
-    return [
-        {
-            name: 'Magnemite',
-            location: {
-                //lat: 4.694047,
-                lat: actualLat + Math.floor((Math.random() * 10)/100000),
-                //lng: -74.065458
-                lng: actualLong + Math.floor((Math.random() * 10)/100000)
-            },
-            source: './assets/magnemite/scene.gltf',
-            scale: '0.3 0.3 0.3'
-        },
-        {
-            name: 'Chocolate',
-            location: {
-                lat: 4.692978,
-                lat: actualLat + Math.floor((Math.random() * 10)/100000),
-                //lng: -74.064948
-                lng: actualLong + Math.floor((Math.random() * 10)/100000)
-            },
-            source: './assets/halloween/chocolate.glb',
-            scale: '0.05 0.05 0.05'
-        }
-    ];
 }
 
 function renderPlaces(places) {
